72bf0c6e74401dc0f45174d67aa1252c4a6d95b3
DieCyberwerkstatt <seslija.sasha@gmail.com>
2023-08-29 22:04:28 +0200

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookingsComponent } from './bookings/bookings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"bookings", component: BookingsComponent
+  },
+  {
+    path:"", redirectTo:"bookings", pathMatch:"full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
