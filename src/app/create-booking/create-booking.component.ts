8bff71d2f66723732628a5dbe84818ffb10b35a4
DieCyberwerkstatt <seslija.sasha@gmail.com>
2023-08-30 22:46:53 +0200

    more code

--- src/app/create-booking/create-booking.component.ts
+++ src/app/create-booking/create-booking.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Booking } from '../booking';
 import { Bookings } from '../mock-bookings';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-create-booking',
@@ -10,25 +10,53 @@ import { Router } from '@angular/router';
 })
 export class CreateBookingComponent implements OnInit {
 
-  constructor(private router: Router){ }
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) { }
 
   booking: Booking = {
     id: 100,
     name: "Your Name",
     roomNumber: 110,
-    startDate: new Date(), 
-    endDate: new Date() 
+    startDate: new Date(),
+    endDate: new Date()
   }
 
   ngOnInit(): void {
-      
+    if (this.router.url != "/create") {
+      let id = Number(this.activatedRoute.snapshot.paramMap.get("id"));
+      let bookingById = Bookings.find(x => x.id == id)!;
+      this.booking = bookingById
+    }
+
+
   }
 
   save(): void {
-    Bookings.push(this.booking)
+    let bookingById = Bookings.find(x => x.id == this.booking.id);
+    if (bookingById == null || bookingById == undefined) {
+      Bookings.push(this.booking)
+    } else {
+      bookingById = this.booking;
+    }
     this.router.navigate(["bookings"])
+
   }
 
- 
+  deleteBooking(){
+    let bookingById = Bookings.findIndex(x => x.id == this.booking.id)!;
+    Bookings.splice(bookingById,1);
+    this.router.navigate(["bookings"])  
+  }
+
+  dateChanged(event: Event, isStart: Boolean) {
+    let val = (event.target as HTMLInputElement).value;
+
+    if (isStart) {
+      this.booking.startDate = new Date(val)
+    } else {
+      this.booking.endDate = new Date(val)
+    }
+  }
+
+
 
 }
--- src/app/mock-bookings.ts
+++ src/app/mock-bookings.ts
@@ -2,28 +2,28 @@ import { Booking } from "./booking";
 
 export const Bookings: Booking[] = [
     {
-        id: 20230245,
+        id: 1,
         name: "Sasha Seslija",
         roomNumber: 100,
         startDate: new Date(),
         endDate: new Date("2024-07-21")
     },
     {
-        id: 20230255,
+        id: 2,
         name: "Hans Meier",
         roomNumber: 130,
         startDate: new Date(),
         endDate: new Date("2024-07-21")
     },
     {
-        id: 20230265,
+        id: 3,
         name: "Peter Tag",
         roomNumber: 120,
         startDate: new Date(),
         endDate: new Date("2024-07-21")
     },
     {
-        id: 20230275,
+        id: 4,
         name: "Thomas Müller",
         roomNumber: 110,
         startDate: new Date(),
